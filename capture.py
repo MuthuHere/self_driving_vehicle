0e4fdc5fd3d44aff518d2d84c22e8901b8f78390
achusat <achyutasatish@gmail.com>
2019-12-18 00:48:46 +0530

    Colour conversion and fixing low fps

--- capture.py
+++ capture.py
@@ -5,11 +5,11 @@ import time
 
 last_time = time.time()
 while(True):
-    printscreen_pil = ImageGrab.grab(bbox=(0, 40, 800, 640))
+    printscreen_pil = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))## converting to np array here to prevent low fps caused by pil
 ##    printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8').reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
     print('Loop took {} second'.format(time.time()-last_time))
     last_time = time.time()
-##    cv2.imshow('window',printscreen_numpy)
-##    if cv2.waitKey(25) & 0xFF == ord('q'):
-##        cv2.destroyAllWindows()
-##        break
+    cv2.imshow('window',cv2.cvtColor(printscreen_pil, cv2.COLOR_BGR2RGB))
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
