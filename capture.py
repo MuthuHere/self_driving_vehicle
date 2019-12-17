ba7718996bb173311cd8ad0caf7ceac9f953e488
achusat <achyutasatish@gmail.com>
2019-12-18 00:33:11 +0530

    Reshaping causes drop in fps

--- capture.py
+++ capture.py
@@ -6,7 +6,7 @@ import time
 last_time = time.time()
 while(True):
     printscreen_pil = ImageGrab.grab(bbox=(0, 40, 800, 640))
-    printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8').reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
+##    printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8').reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
     print('Loop took {} second'.format(time.time()-last_time))
     last_time = time.time()
 ##    cv2.imshow('window',printscreen_numpy)
