83baa7ff51694d7c7a0f9b4510d5a8906d02c77c
achusat <achyutasatish@gmail.com>
2019-12-18 17:31:22 +0530

    Key inputs for game

--- capture.py
+++ capture.py
@@ -3,20 +3,22 @@ import cv2
 from PIL import ImageGrab
 import time
 
+for i in list(range(4))[::-1]:
+    print(i+1)
+    time.sleep(1)
+
 def process_image(original_image):
     processed_image = cv2.cvtColor(original_image, cv2.COLOR_BGRA2GRAY)
     processed_image = cv2.Canny(processed_image, threshold1=200, threshold2=300)
     return processed_image
 
-last_time = time.time()
-while(True):
-    screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))## converting to np array here to prevent low fps caused by pil
-    edge_screen = process_image(screen)
+##last_time = time.time()
+##while(True):
+##    screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))## converting to np array here to prevent low fps caused by pil
+##    edge_screen = process_image(screen)
 ##    printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8').reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
-    print('Loop took {} second'.format(time.time()-last_time))
-    last_time = time.time()
-    cv2.imshow('window1',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-    cv2.imshow('window2', edge_screen)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+##    print('Loop took {} second'.format(time.time()-last_time))
+##    cv2.imshow('window1',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+##    cv2.imshow('window2', edge_screen)
+##    if cv2.waitKey(25) & 0xFF == ord('q'):
+##        break
