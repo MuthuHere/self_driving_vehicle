26fcca3f7ca175c912610a39d37465d01b32089b
achusat <achyutasatish@gmail.com>
2019-12-24 18:36:14 +0530

    Finding the sinificant lines (Hough lines)

--- capture.py
+++ capture.py
@@ -16,11 +16,23 @@ def roi(image, vertices):
     masked = cv2.bitwise_and(image,mask)
     return masked
 
+#drawing the significant lines on the image
+def draw_lines(image, lines):
+    try:
+        for line in lines:
+            coords = line[0]
+            cv2.line(image, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)
+    except:
+        pass
+
 def process_image(original_image):
     processed_image = cv2.cvtColor(original_image, cv2.COLOR_BGRA2GRAY)
     processed_image = cv2.Canny(processed_image, threshold1=200, threshold2=300)
-    vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500]])
+    vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500]], np.int32)
+    processed_image = cv2.GaussianBlur(processed_image, (3,3), 0) #anti aliasing
     processed_image = roi(processed_image, [vertices])
+    lines = cv2.HoughLinesP(processed_image, 1, np.pi/180, 180, np.array([]), 100, 5)
+    draw_lines(processed_image, lines)
     return processed_image
 
 last_time = time.time()
@@ -34,6 +46,7 @@ while(True):
     ##print('key up')
     ##ReleaseKey(W)
     print('Loop took {} second'.format(time.time()-last_time))
+    last_time = time.time()
     cv2.imshow('window1',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
     cv2.imshow('window2', edge_screen)
     if cv2.waitKey(25) & 0xFF == ord('q'):
