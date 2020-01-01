1f8abe9733cef02c570e6444d96fd6356a6c5ab5
achusat <achyutasatish@gmail.com>
2020-01-01 17:36:50 +0530

    Using pywin32 to capture the screen instead of PIL

--- capture.py
+++ capture.py
@@ -1,160 +1,35 @@
-import numpy as np
 import cv2
-from PIL import ImageGrab
-import time
-from directinput import W, A, S, D, PressKey, ReleaseKey
-import pyautogui#controls window size
-from numpy import ones,vstack
-from numpy.linalg import lstsq
-from statistics import mean
+import win32gui, win32ui, win32con, win32api
+import numpy as np
 
-##for i in list(range(7))[::-1]:
-##    print(i+1)
-##    time.sleep(1)
+def cap(region=None):
+    hwin = win32gui.GetDesktopWindow()
 
-#defining a region of interest to prevent unnecessary data
-def roi(image, vertices):
-    mask = np.zeros_like(image)
-    cv2.fillPoly(mask, vertices, 255)
-    masked = cv2.bitwise_and(image,mask)
-    return masked
+    if region:
+        left, top, x2, y2 = region
+        width = x2 - left + 1
+        height = y2 - top + 1
+    else:
+        width = win32api.getSystemMetric(win32con.SM_CXVIRTUALSCREEN)
+        height = win32api.getSystemMetric(win32con.SM_CYVITUALSCREEN)
+        left = win32api.getSystemMetric(win32con.SM_XVIRTUALSCREEN)
+        top = win32api.getSystemMetric(win32con.SM_YVIRTUALSCREEN)
 
-#drawing the significant lines on the image
-def draw_lines(image, lines):
-    try:
-        for line in lines:
-            coords = line[0]
-            cv2.line(image, (coords[0], coords[1]), (coords[2], coords[3]), [255,255,255], 3)
-    except:
-        pass
+    hwindc = win32gui.GetWindowDC(hwin)
+    srcdc = win32ui.CreateDCFromHandle(hwindc)
+    memdc = srcdc.CreateCompatibleDC()
+    bmp = win32.CreateBitmap()
+    bmp.CreateCompatibleBitmap(srcdc, height, width)
+    memdc.SelectObject(bmp)
+    memdc.BitBlt((0,0), (width,height), srcdc, (left,top), win32con.SRCCOPY)
 
-def process_image(image):
-    original_image = image
-    processed_image = cv2.cvtColor(original_image, cv2.COLOR_BGRA2GRAY)
-    processed_image = cv2.Canny(processed_image, threshold1=200, threshold2=300)
-    processed_image = cv2.GaussianBlur(processed_image, (5,5), 0) #anti aliasing
-    vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500]], np.int32)
-    processed_image = roi(processed_image, [vertices])
-    lines = cv2.HoughLinesP(processed_image, 1, np.pi/180, 180, np.array([]), 20, 15)
-    try:
-        l1,l2 = draw_lanes(original_image,lines)
-        cv2.line(original_image, (l1[0], l1[1]), (l1[2], l1[3]), [0,255,0], 30)
-        cv2.line(original_image, (l2[0], l2[1]), (l2[2], l2[3]), [0,255,0], 30)
-    except Exception as e:
-        print(str(e))
-        pass
-    try:
-        for coords in lines:
-            coords = coords[0]
-            try:
-                cv2.line(processed_image, (coords[0], coords[1]), (coords[2], coords[3]), [255,0,0], 3)
-            except Exception as e:
-                print(str(e))
-    except Exception as e:
-        pass
+    signedIntsArray = bmp.getBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype='uint8')
+    img.shape = (height,width,4)
 
-    return processed_image,original_image
+    srcdc.DeleteDC()
+    memdc.DeleteDC()
+    win32gui.ReleaseDC(hwin, hwindc)
+    win32gui.DeleteObject(bmp.GetHandle())
 
-def draw_lanes(img, lines, color=[0, 255, 255], thickness=3):
-
-    # if this fails, go with some default line
-    try:
-
-        # finds the maximum y value for a lane marker
-        # (since we cannot assume the horizon will always be at the same point.)
-
-        ys = []
-        for i in lines:
-            for ii in i:
-                ys += [ii[1],ii[3]]
-        min_y = min(ys)
-        max_y = 600
-        new_lines = []
-        line_dict = {}
-
-        for idx,i in enumerate(lines):
-            for xyxy in i:
-                # These four lines:
-                # Used to calculate the definition of a line, given two sets of coords.
-                x_coords = (xyxy[0],xyxy[2])
-                y_coords = (xyxy[1],xyxy[3])
-                A = vstack([x_coords,ones(len(x_coords))]).T
-                m, b = lstsq(A, y_coords)[0]
-
-                # Calculating our new, and improved, xs
-                x1 = (min_y-b) / m
-                x2 = (max_y-b) / m
-
-                line_dict[idx] = [m,b,[int(x1), min_y, int(x2), max_y]]
-                new_lines.append([int(x1), min_y, int(x2), max_y])
-
-        final_lanes = {}
-
-        for idx in line_dict:
-            final_lanes_copy = final_lanes.copy()
-            m = line_dict[idx][0]
-            b = line_dict[idx][1]
-            line = line_dict[idx][2]
-
-            if len(final_lanes) == 0:
-                final_lanes[m] = [ [m,b,line] ]
-
-            else:
-                found_copy = False
-
-                for other_ms in final_lanes_copy:
-
-                    if not found_copy:
-                        if abs(other_ms*1.2) > abs(m) > abs(other_ms*0.8):
-                            if abs(final_lanes_copy[other_ms][0][1]*1.2) > abs(b) > abs(final_lanes_copy[other_ms][0][1]*0.8):
-                                final_lanes[other_ms].append([m,b,line])
-                                found_copy = True
-                                break
-                        else:
-                            final_lanes[m] = [ [m,b,line] ]
-
-        line_counter = {}
-
-        for lanes in final_lanes:
-            line_counter[lanes] = len(final_lanes[lanes])
-
-        top_lanes = sorted(line_counter.items(), key=lambda item: item[1])[::-1][:2]
-
-        lane1_id = top_lanes[0][0]
-        lane2_id = top_lanes[1][0]
-
-        def average_lane(lane_data):
-            x1s = []
-            y1s = []
-            x2s = []
-            y2s = []
-            for data in lane_data:
-                x1s.append(data[2][0])
-                y1s.append(data[2][1])
-                x2s.append(data[2][2])
-                y2s.append(data[2][3])
-            return int(mean(x1s)), int(mean(y1s)), int(mean(x2s)), int(mean(y2s))
-
-        l1_x1, l1_y1, l1_x2, l1_y2 = average_lane(final_lanes[lane1_id])
-        l2_x1, l2_y1, l2_x2, l2_y2 = average_lane(final_lanes[lane2_id])
-
-        return [l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2]
-    except Exception as e:
-        print(str(e))
-
-last_time = time.time()
-while(True):
-    screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))# converting to np array here to prevent low fps caused by pil
-    edge_screen, original_screen = process_image(screen)
-    #pressing and releasing key
-    ##print('key down')
-    ##PressKey(W)
-    ##time.sleep(3)
-    ##print('key up')
-    ##ReleaseKey(W)
-    print('Loop took {} second'.format(time.time()-last_time))
-    last_time = time.time()
-    cv2.imshow('window1',cv2.cvtColor(original_screen, cv2.COLOR_BGR2RGB))
-    cv2.imshow('window2', edge_screen)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
+    return cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
