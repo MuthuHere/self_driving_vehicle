f89930aedbded5a4fb7b1edffd637e7120cf6cd0
achusat <achyutasatish@gmail.com>
2020-01-16 16:48:46 +0530

    Restructured code

--- capture.py
+++ capture.py
@@ -1,29 +1,31 @@
 import cv2
-import win32gui, win32ui, win32con, win32api
 import numpy as np
+import win32gui, win32ui, win32con, win32api
 
-def cap(region=None):
+def grab_screen(region=None):
+
     hwin = win32gui.GetDesktopWindow()
 
     if region:
-        left, top, x2, y2 = region
-        width = x2 - left + 1
-        height = y2 - top + 1
+            left,top,x2,y2 = region
+            width = x2 - left + 1
+            height = y2 - top + 1
     else:
-        width = win32api.getSystemMetric(win32con.SM_CXVIRTUALSCREEN)
-        height = win32api.getSystemMetric(win32con.SM_CYVITUALSCREEN)
-        left = win32api.getSystemMetric(win32con.SM_XVIRTUALSCREEN)
-        top = win32api.getSystemMetric(win32con.SM_YVIRTUALSCREEN)
+        width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+        height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
+        left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
+        top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+
 
     hwindc = win32gui.GetWindowDC(hwin)
     srcdc = win32ui.CreateDCFromHandle(hwindc)
     memdc = srcdc.CreateCompatibleDC()
-    bmp = win32.CreateBitmap()
-    bmp.CreateCompatibleBitmap(srcdc, height, width)
+    bmp = win32ui.CreateBitmap()
+    bmp.CreateCompatibleBitmap(srcdc, width, height)
     memdc.SelectObject(bmp)
-    memdc.BitBlt((0,0), (width,height), srcdc, (left,top), win32con.SRCCOPY)
+    memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
 
-    signedIntsArray = bmp.getBitmapBits(True)
+    signedIntsArray = bmp.GetBitmapBits(True)
     img = np.fromstring(signedIntsArray, dtype='uint8')
     img.shape = (height,width,4)
 
--- lanes.py
+++ lanes.py
@@ -3,44 +3,58 @@ from numpy.linalg import lstsq
 from statistics import mean
 import numpy as np
 
-def draw_lanes(img, lines, color=[0,255,255], thickness=3):
-    #if no lines found
-    try:
-        ys = []
-        for line in lines:
-            for xy in line:
-                ys += [xy[1],xy[3]]
-        min_y = min(ys)
-        max_y = 600 #horizon
+def draw_lanes(img, lines, color=[0, 255, 255], thickness=3):
 
+    # if this fails, go with some default line
+    try:
+
+        # finds the maximum y value for a lane marker
+        # (since we cannot assume the horizon will always be at the same point.)
+
+        ys = []
+        for i in lines:
+            for ii in i:
+                ys += [ii[1],ii[3]]
+        min_y = min(ys)
+        max_y = 600
+        new_lines = []
         line_dict = {}
 
-        for id,line in enumerate(lines):
-            for xy in line:
-                x_coords = (xy[0], xy[2])
-                y_coords = (xy[1], xy[3])
+        for idx,i in enumerate(lines):
+            for xyxy in i:
+                # These four lines:
+                # modified from http://stackoverflow.com/questions/21565994/method-to-return-the-equation-of-a-straight-line-given-two-points
+                # Used to calculate the definition of a line, given two sets of coords.
+                x_coords = (xyxy[0],xyxy[2])
+                y_coords = (xyxy[1],xyxy[3])
                 A = vstack([x_coords,ones(len(x_coords))]).T
-                m, b = lstsq(a, y_coords)[0]
+                m, b = lstsq(A, y_coords)[0]
 
-                #improved x calculation
-                x1 = (min_y-b)/m
-                x2 = (max_y-b)/m
+                # Calculating our new, and improved, xs
+                x1 = (min_y-b) / m
+                x2 = (max_y-b) / m
 
-                line_dict[id] = [m,b,[int(x1),min_y,int(x2),max_y]]
+                line_dict[idx] = [m,b,[int(x1), min_y, int(x2), max_y]]
+                new_lines.append([int(x1), min_y, int(x2), max_y])
 
         final_lanes = {}
-        for id in line_dict:
+
+        for idx in line_dict:
             final_lanes_copy = final_lanes.copy()
-            m = line_dict[id][0]
-            b = line_dict[id][1]
-            line = line_dict[id][2]
+            m = line_dict[idx][0]
+            b = line_dict[idx][1]
+            line = line_dict[idx][2]
+
             if len(final_lanes) == 0:
-                final_lanes[m] = [[m, b, line]]
+                final_lanes[m] = [ [m,b,line] ]
+
             else:
-                found_copy = false
+                found_copy = False
+
                 for other_ms in final_lanes_copy:
+
                     if not found_copy:
-                        if abs(other_ms*1.2)>abs(m)>abs(other_ms*0.8):
+                        if abs(other_ms*1.2) > abs(m) > abs(other_ms*0.8):
                             if abs(final_lanes_copy[other_ms][0][1]*1.2) > abs(b) > abs(final_lanes_copy[other_ms][0][1]*0.8):
                                 final_lanes[other_ms].append([m,b,line])
                                 found_copy = True
